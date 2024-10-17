00f62017e99fb862b8e4cb0aefe7f2302b8a8a97
MichelleChinYeeLin <michellechinyl@gmail.com>
2024-10-17 12:01:17 +0800

    Add logic to move characters between rooms based on dice color

--- src/costumePartyDetective.js
+++ src/costumePartyDetective.js
@@ -10,7 +10,8 @@ class Character {
     this.isPlayer = false;
     this.playerId = -1;
     this.isAlive = true;
-    this.currentRoom = "";
+    this.currentRoomId = -1;
+    this.currentRoomColor = "";
   }
 }
 
@@ -78,7 +79,7 @@ function Game({ props }) {
   );
   const [playerList, setPlayerList] = useState([]);
   const [allCharacterList, setAllCharacterList] = useState([]);
-  const roomColorList = ["red", "green", "blue", "mid", "yellow"];
+  const roomColorList = ["RED", "GREEN", "BLUE", "MID", "YELLOW"];
   const [roomList, setRoomList] = useState([]);
   const [gameStatus, setGameStatus] = useState("SETUP");
   const [currentPlayerCardReveal, setCurrentPlayerCardReveal] = useState(undefined);
@@ -87,6 +88,8 @@ function Game({ props }) {
   const [playerSequence, setPlayerSequence] = useState(
     Array.from({ length: playerNum }, (_value, index) => index)
   );
+  const [diceRollColor, setDiceRollColor] = useState("");
+  const [selectedCharacter, setSelectedCharacter] = useState(-1);
 
   const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
@@ -113,7 +116,7 @@ function Game({ props }) {
 
   useEffect(() => {
     // Initialize rooms when all characters are set
-    if (allCharacterList.length > 0) {
+    if (gameStatus === "SETUP" && allCharacterList.length > 0) {
       let availableRoomList = Array.from(roomColorList, (_value, index) => new Room(index));
       let tempRoomList = [];
       let tempCharacterList = [...allCharacterList];
@@ -121,9 +124,9 @@ function Game({ props }) {
 
       // Assign all characters to rooms
       tempCharacterList.forEach((character) => {
-        if (character.currentRoom === "") {
+        if (character.currentRoomId == -1) {
           let rngRoomIndex = Math.floor(Math.random() * availableRoomList.length);
-          character.currentRoom = availableRoomList[rngRoomIndex].color;
+          character.currentRoomId = availableRoomList[rngRoomIndex].id;
           availableRoomList[rngRoomIndex].capacity++;
           availableRoomList[rngRoomIndex].characterList.push(character);
 
@@ -176,32 +179,153 @@ function Game({ props }) {
     } else if (gameStatus === "START") {
       async function showGameStartCard() {
         await sleep(2000);
+        setGameStatus("PLAYER_TURN_START");
+      }
+      showGameStartCard();
+    } else if (gameStatus === "PLAYER_TURN_START") {
+      async function showPlayerTurnCard() {
+        await sleep(2000);
+        colorDiceRoll();
+        setGameStatus("PLAYER_TURN_DICE_ROLL");
+      }
+      showPlayerTurnCard();
+    } else if (gameStatus === "PLAYER_TURN_DICE_ROLL") {
+      async function showDiceRollCard() {
+        await sleep(3000);
         setGameStatus("PLAYER_TURN");
       }
-
-      showGameStartCard();
+      showDiceRollCard();
     }
   }, [gameStatus]);
+
+  useEffect(() => {}, [playerSequence]);
+
+  useEffect(() => {
+    console.log(diceRollColor);
+  }, [diceRollColor]);
+
+  function colorDiceRoll() {
+    let diceRollColors = ["RED", "GREEN", "BLUE", "YELLOW", "BLACK", "BLACK"];
+    let diceRoll = Math.floor(Math.random() * 6);
+    setDiceRollColor(diceRollColors[diceRoll]);
+  }
+
+  function onCharacterSelected(event) {
+    if (gameStatus !== "PLAYER_TURN") {
+      return;
+    }
+
+    let newSelectedCharacter = event.target.getAttribute("character-id");
+
+    if (selectedCharacter === newSelectedCharacter) {
+      setSelectedCharacter(-1);
+      // console.log("ehh");
+    } else {
+      setSelectedCharacter(newSelectedCharacter);
+      // console.log(newSelectedCharacter);
+    }
+  }
+
+  useEffect(() => {
+    // console.log(selectedCharacter);
+  }, [selectedCharacter]);
+
+  function onRoomSelected(event) {
+    if (gameStatus === "PLAYER_TURN" && selectedCharacter !== -1 && diceRollColor !== "BLACK") {
+      let selectedRoomId = event.currentTarget.getAttribute("room-id");
+
+      // Validate if character can be moved to selected room
+      let tempAllCharacterList = [...allCharacterList];
+      let tempRoomList = [...roomList];
+      let selectedRoom = undefined;
+
+      tempRoomList.forEach((room) => {
+        if (room.id == selectedRoomId) {
+          selectedRoom = room;
+        }
+      });
+
+      tempAllCharacterList.forEach((character) => {
+        if (character.id == selectedCharacter) {
+          let currentRoom = character.currentRoomId;
+          let currentRoomColor = "";
+
+          tempRoomList.forEach((room) => {
+            if (room.id === currentRoom) {
+              currentRoomColor = room.color;
+            }
+          });
+
+          if (
+            (currentRoomColor === "RED" && selectedRoom.color === "YELLOW") ||
+            (currentRoomColor === "YELLOW" && selectedRoom.color === "RED") ||
+            (currentRoomColor === "BLUE" && selectedRoom.color === "GREEN") ||
+            (currentRoomColor === "GREEN" && selectedRoom.color === "BLUE") ||
+            (currentRoomColor !== diceRollColor && selectedRoom.color !== diceRollColor)
+          ) {
+            console.log("invalid move");
+          } else {
+            tempRoomList.forEach((room) => {
+              if (room.id === selectedRoom.id) {
+                room.characterList.push(character);
+                room.capacity++;
+              } else if (room.id === character.currentRoomId) {
+                let characterIndex = 0;
+                room.characterList.forEach((roomCharacter, index) => {
+                  if (roomCharacter.id === character.id) {
+                    characterIndex = index;
+                  }
+                });
+
+                room.characterList.splice(characterIndex, 1);
+                room.capacity--;
+              }
+            });
+            character.currentRoomId = selectedRoom.id;
+          }
+        }
+      });
+
+      setAllCharacterList(tempAllCharacterList);
+      setSelectedCharacter(-1);
+      setRoomList(tempRoomList);
+    }
+  }
+
+  useEffect(() => {
+    // console.log(roomList);
+  }, [roomList]);
 
   return (
     <div className="h-full w-full flex justify-center items-center bg-gray-200">
       <div className="h-[80%] w-[80%] grid grid-cols-3 gap-0">
-        {roomList.map((room) => (
+        {roomList.map((room, index) => (
           <div
-            key={"room: " + room.color}
-            className={`flex flex-wrap justify-center items-center ${
-              room.color === "red"
+            key={index}
+            room-color={room.color}
+            room-id={room.id}
+            className={`flex flex-wrap justify-center items-center content-center border border-black ${
+              room.color === "RED"
                 ? "col-span-2 bg-red-300"
-                : room.color === "green"
+                : room.color === "GREEN"
                 ? "row-span-2 bg-lime-300"
-                : room.color === "blue"
+                : room.color === "BLUE"
                 ? "row-span-2 bg-blue-300"
-                : room.color === "mid"
+                : room.color === "MID"
                 ? "bg-gray-300"
                 : "col-span-2 bg-yellow-300"
-            }`}>
+            }`}
+            onClick={onRoomSelected}>
             {room.characterList.map((character) => (
-              <div className="h-[3rem] w-[3rem] bg-white text-black text-lg border border-black rounded-lg flex justify-center items-center p-2 m-2 hover:bg-gray-200">
+              <div
+                key={character.id}
+                className={`h-[3rem] w-[3rem] text-lg border border-black rounded-lg flex justify-center items-center p-2 m-2 ${
+                  selectedCharacter == character.id
+                    ? "bg-black text-white"
+                    : "bg-white text-black hover:bg-gray-200"
+                }`}
+                character-id={character.id}
+                onClick={onCharacterSelected}>
                 {character.id}
               </div>
             ))}
@@ -238,7 +362,23 @@ function Game({ props }) {
         gameStatus === "START" ? (
           <div className="h-full w-full absolute top-0 left-0 flex justify-center items-center">
             <div className="h-full w-full absolute top-0 left-0 fixed bg-gray-500 opacity-30 z-[10]"></div>
-            <div className="h-1/2 w-1/2 bg-white flex justify-center items-center text-[4rem] font-bold rounded-xl p-10 z-[20]">GAME START</div>
+            <div className="h-1/2 w-1/2 bg-white flex justify-center items-center text-[4rem] font-bold rounded-xl p-10 z-[20]">
+              GAME START
+            </div>
+          </div>
+        ) : gameStatus === "PLAYER_TURN_START" ? (
+          <div className="h-full w-full absolute top-0 left-0 flex justify-center items-center">
+            <div className="h-full w-full absolute top-0 left-0 fixed bg-gray-500 opacity-30 z-[10]"></div>
+            <div className="h-1/2 w-1/2 bg-white flex justify-center items-center text-[4rem] font-bold rounded-xl p-10 z-[20] text-center">
+              {"PLAYER " + (playerSequence[0] + 1) + "'S TURN"}
+            </div>
+          </div>
+        ) : gameStatus === "PLAYER_TURN_DICE_ROLL" ? (
+          <div className="h-full w-full absolute top-0 left-0 flex justify-center items-center">
+            <div className="h-full w-full absolute top-0 left-0 fixed bg-gray-500 opacity-30 z-[10]"></div>
+            <div className="h-1/2 w-1/2 bg-white flex justify-center items-center text-[4rem] font-bold rounded-xl p-10 z-[20] text-center">
+              {"DICE ROLL: " + diceRollColor}
+            </div>
           </div>
         ) : (
           <></>
