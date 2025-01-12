8ac97a0037fcadb6c3a6c62c8f0c6fcb2147357e
MichelleChinYeeLin <michellechinyl@gmail.com>
2025-01-12 18:30:38 +0800

    Add lobby.js file and change landing page to lobby

---
+++ client/src/lobby.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import "./index.css";
+
+const Lobby = () => {
+  const gameInformationList = [
+    {
+      name: "Costume Party Detective",
+      description:
+        "You have been invited to a costume party. Because of the elaborate costumers, you do not know anyone's identity except your own. Be the last player standing.",
+      imageUrl: "/assets/images/costume-party-detective-background.png",
+    },
+  ];
+
+  const [isShowingJoinDetails, setIsShowingJoinDetails] = useState(false);
+  const [roomCode, setRoomCode] = useState("");
+
+  function ToggleDetailsContainer(event) {
+    const containerType = event.target.getAttribute("data-buttontype");
+    if (containerType === "create") {
+      setIsShowingJoinDetails(false);
+    } else {
+      setIsShowingJoinDetails(!isShowingJoinDetails);
+      setRoomCode("");
+    }
+  }
+
+  function CheckLobbyRoomCode(event) {
+    const code = event.target.value;
+    setRoomCode(code.toUpperCase());
+  }
+
+  return (
+    <div className={`h-full w-full flex flex-col justify-center items-center overflow-auto`}>
+      <h1 className="cursive">Party Games</h1>
+      <div className="content">
+        <h2>
+          <span>LOBBY</span>
+        </h2>
+        <div className="w-1/2 flex justify-center items-center my-3">
+          <button
+            className="w-[10rem] bg-green-700 py-3 px-5 mx-1 rounded-lg text-white text-lg"
+            type="button"
+            data-buttontype="create"
+            onClick={ToggleDetailsContainer}>
+            CREATE
+          </button>
+          <button
+            className="w-[10rem] bg-blue-600 py-3 px-5 mx-1 rounded-lg text-white text-lg"
+            type="button"
+            data-buttontype="join"
+            onClick={ToggleDetailsContainer}>
+            JOIN
+          </button>
+          <div className="h-[2rem] w-[2rem] absolute bg-white rounded-full flex justify-center items-center">
+            or
+          </div>
+        </div>
+        <h2>
+          <span>GAME INFORMATION</span>
+        </h2>
+        <div id="gameInformationContainer" className="w-3/4 flex flex-wrap justify-center">
+          {gameInformationList.map((gameInfo, index) => (
+            <div className="h-[25rem] w-[25rem] shadow items-center flex flex-col rounded-xl mx-5 my-3 bg-white" key={index}>
+              <div className="h-[50%] w-full">
+                <img
+                  src={gameInfo.imageUrl}
+                  className="h-full w-full rounded-tl-xl rounded-tr-xl object-cover"></img>
+              </div>
+              <div className="h-[50%] w-full px-4 py-5 flex flex-col">
+                <span className="card-title text-lg font-bold text-left py-1">{gameInfo.name}</span>
+                <span className="card-description text-md text-left py-1">
+                  {gameInfo.description}
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Modal */}
+      {
+        isShowingJoinDetails ? <div className="h-screen w-screen absolute flex justify-center items-center">
+          <div className="modal-backdrop"></div>
+          <div className="w-[40%] bg-[#F5F5F5] p-5 rounded-lg z-[200] relative flex flex-col">
+            <button className="absolute p-3 top-0 right-0" data-buttontype="close" onClick={ToggleDetailsContainer}>&#10006;</button>
+            <span className="w-full text-center font-bold text-3xl mt-5 mb-8">LOBBY DETAILS</span>
+            <div className="h-fit-content w-full flex flex-col">
+              <span className="font-bold text-lg px-3 py-2">ROOM CODE</span>
+              <input type="text" className="h-fit-content w-full bg-white py-2 px-3 border border-gray-400 rounded-lg focus:outline-none" placeholder="ENTER ROOM CODE" onChange={CheckLobbyRoomCode} value={roomCode}></input>
+            </div>
+            <button className="w-full font-bold text-lg text-white px-5 py-2 bg-blue-600 mt-4 rounded-lg">JOIN</button>
+          </div>
+        </div> : <></>
+      }
+    </div>
+  );
+};
+
+export default Lobby;
